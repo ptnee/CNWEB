aa042deb5edc00842be1a0145bb8f1819a42ccc3
Phạm Thắm <phamtham@MacBook-Air-cua-Pham-3.local>
2023-10-29 22:09:52 +0700

    user sign up, login, logout

---
+++ BE/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const routes = require('./routes')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+routes(app);
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+    })
+    .catch((err) => {
+    })
+app.listen(port, () => {
+    console.log('Server is running in port: ', + port)
+})
---
+++ BE/src/services/UserService.js
@@ -0,0 +1,104 @@
+const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService")
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser !== null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The email is already'
+                })
+            }
+            const hash = bcrypt.hashSync(password, 10)
+            const createdUser = await User.create({
+                name,
+                email,
+                password: hash,
+                phone
+            })
+            if (createdUser) {
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: createdUser
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { email, password } = userLogin
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+
+            if (!comparePassword) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The password or user is incorrect'
+                })
+            }
+            const access_token = await genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            const refresh_token = await genneralRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                access_token,
+                refresh_token
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateUser = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedUser
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
