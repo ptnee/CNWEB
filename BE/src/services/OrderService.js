189b396ce4698cf86e7c4e00df6e570d3ead9bb5
ManhThang248 <manhthang24082000@gmail.com>
2023-11-15 19:28:30 +0700

    fix

--- BE/src/services/OrderService.js
+++ BE/src/services/OrderService.js
@@ -73,19 +73,6 @@ const createOrder = (newOrder) => {
     })
 }
 
-// const deleteManyProduct = (ids) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             await Product.deleteMany({ _id: ids })
-//             resolve({
-//                 status: 'OK',
-//                 message: 'Delete product success',
-//             })
-//         } catch (e) {
-//             reject(e)
-//         }
-//     })
-// }
 
 const getAllOrderDetails = (id) => {
     return new Promise(async (resolve, reject) => {
@@ -107,98 +94,6 @@ const getAllOrderDetails = (id) => {
             })
         } catch (e) {
             // console.log('e', e)
-            reject(e)
-        }
-    })
-}
-
-const getOrderDetails = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const order = await Order.findById({
-                _id: id
-            })
-            if (order === null) {
-                resolve({
-                    status: 'ERR',
-                    message: 'The order is not defined'
-                })
-            }
-
-            resolve({
-                status: 'OK',
-                message: 'SUCESSS',
-                data: order
-            })
-        } catch (e) {
-            // console.log('e', e)
-            reject(e)
-        }
-    })
-}
-
-const cancelOrderDetails = (id, data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let order = []
-            const promises = data.map(async (order) => {
-                const productData = await Product.findOneAndUpdate(
-                    {
-                    _id: order.product,
-                    selled: {$gte: order.amount}
-                    },
-                    {$inc: {
-                        countInStock: +order.amount,
-                        selled: -order.amount
-                    }},
-                    {new: true}
-                )
-                if(productData) {
-                    order = await Order.findByIdAndDelete(id)
-                    if (order === null) {
-                        resolve({
-                            status: 'ERR',
-                            message: 'The order is not defined'
-                        })
-                    }
-                } else {
-                    return{
-                        status: 'OK',
-                        message: 'ERR',
-                        id: order.product
-                    }
-                }
-            })
-            const results = await Promise.all(promises)
-            const newData = results && results[0] && results[0].id
-            
-            if(newData) {
-                resolve({
-                    status: 'ERR',
-                    message: `San pham voi id: ${newData} khong ton tai`
-                })
-            }
-            resolve({
-                status: 'OK',
-                message: 'success',
-                data: order
-            })
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
-
-const getAllOrder = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const allOrder = await Order.find().sort({createdAt: -1, updatedAt: -1})
-            resolve({
-                status: 'OK',
-                message: 'Success',
-                data: allOrder
-            })
-        } catch (e) {
             reject(e)
         }
     })
