5bd83cc7f8e69014904953dbd0d71a2e558fb00a
ManhThang248 <manhthang24082000@gmail.com>
2023-11-22 22:02:16 +0700

    add getallorder cancelorder getorderDtails

--- BE/src/services/OrderService.js
+++ BE/src/services/OrderService.js
@@ -98,7 +98,95 @@ const getAllOrderDetails = (id) => {
         }
     })
 }
+const getOrderDetails = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const order = await Order.findById({
+                _id: id
+            })
+            if (order === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The order is not defined'
+                })
+            }
 
+            resolve({
+                status: 'OK',
+                message: 'SUCESSS',
+                data: order
+            })
+        } catch (e) {
+            // console.log('e', e)
+            reject(e)
+        }
+    })
+}
+const cancelOrderDetails = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let order = []
+            const promises = data.map(async (order) => {
+                const productData = await Product.findOneAndUpdate(
+                    {
+                    _id: order.product,
+                    selled: {$gte: order.amount}
+                    },
+                    {$inc: {
+                        countInStock: +order.amount,
+                        selled: -order.amount
+                    }},
+                    {new: true}
+                )
+                if(productData) {
+                    order = await Order.findByIdAndDelete(id)
+                    if (order === null) {
+                        resolve({
+                            status: 'ERR',
+                            message: 'The order is not defined'
+                        })
+                    }
+                } else {
+                    return{
+                        status: 'OK',
+                        message: 'ERR',
+                        id: order.product
+                    }
+                }
+            })
+            const results = await Promise.all(promises)
+            const newData = results && results[0] && results[0].id
+            
+            if(newData) {
+                resolve({
+                    status: 'ERR',
+                    message: `San pham voi id: ${newData} khong ton tai`
+                })
+            }
+            resolve({
+                status: 'OK',
+                message: 'success',
+                data: order
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+const getAllOrder = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allOrder = await Order.find().sort({createdAt: -1, updatedAt: -1})
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allOrder
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 module.exports = {
     createOrder,
     getAllOrderDetails,
