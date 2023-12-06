73b03effa4c26b1c6be39ffff7c43c247910365a
PhamDucThang10 <phamthang05092002@gmail.com>
2023-12-06 23:37:08 +0700

    upcode myorder

---
+++ src/components/StepConponent/styled.js
@@ -0,0 +1,10 @@
+import { Steps } from "antd";
+import styled from "styled-components";
+const { Step } = Steps;
+
+
+export const CustomStep = styled(Step)`
+  .ant-steps-item-process>.ant-steps-item-container>.ant-steps-item-icon {
+    background: #e11b1e
+  }
+`
